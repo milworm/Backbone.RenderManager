b88f31b3149d4dcbaa4237bd4a842b1dd734d91a
Ruslan Prytula <rpritula@takeforce.com>
2012-08-02 02:06:01 +0300

    init

---
+++ coreView.js
@@ -0,0 +1,53 @@
+/**
+ * @author Ruslan Prytula
+ * Base class for all application's views
+ */
+
+define(function() {
+
+	return Backbone.View.extend({
+
+		/**
+		 * function clears(removes all listeners) for all views 
+		 * that are currently lies under this.el
+		 * @returns {undefined}
+		 */
+		beforeRender: function() {
+			Backbone.RenderManager.removeUnder(this);
+		},
+
+		/**
+		 * @returns {undefined}
+		 */
+		render: function() {
+			this.beforeRender.apply(this, arguments);
+			this.doRender.apply(this, arguments);
+			this.afterRender.apply(this, arguments);
+		},
+
+		/**
+		 * should be overriden in subclasses
+		 * @returns {undefined}
+		 */
+		doRender: function() {},
+
+		/**
+		 * registers current view in renderManager
+		 */
+		afterRender: function() {
+			Backbone.RenderManager.register(this);
+		},
+
+		unbind: function() {
+			this.undelegateEvents();
+		},
+
+		mask: function() {
+			(this.maskEl ? this.$el.find(this.maskEl) : this.$el).mask();
+        },
+
+        unmask: function() {
+            (this.maskEl ? this.$el.find(this.maskEl) : this.$el).unmask();
+        },
+	});
+});
