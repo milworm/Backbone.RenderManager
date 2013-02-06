dbf9e5707c1fde3df106dc03628675c668027985
Ruslan Prytula <rpritula@takeforce.com>
2013-02-06 19:47:44 +0200

    updated

--- example.js
+++ example.js
@@ -2,10 +2,33 @@ require([
 	"CoreView",
 	"RenderManager"
 ], function(View) {
-	return View.extend({
+	var v1 = View.extend({
 		el: "body",
+		events: {
+			"click .first-button": function() {
+				alert("View 'v1' click handler");
+			}
+		},
 		doRender: function() {
-			$(this.el).html("some html goes here");
+			$(this.el).html('<input type="button" class="first-button" value="Click me" /> some html 1 goes here');
 		}
-	})
+	});
+
+	var v2 = View.extend({
+		el: "body",
+		events: {
+			"click .first-button": function() {
+				alert("View 'v2' click handler");
+			}
+		},
+		doRender: function() {
+			$(this.el).html('<input type="button" class="first-button" value="Click me" />some html 2 goes here');
+		}
+	});
+
+	(new v1).render();
+
+	setTimeout(function() {
+		(new v2).render(); // renders second view and automatically removes View "v1" and unbinds events
+	}, 5000);
 });
